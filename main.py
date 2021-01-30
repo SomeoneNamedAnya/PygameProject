9f530c14045e9d0e6abc8a7b9c65d2174c40c7ec
ForestGameMain <Forest_game_channele@mail.ru>
2021-01-30 21:09:38 +0300

    Create betta version of Start menu

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import sys
+import pygame
+import pygame_menu
 
-class HomePage(QMainWindow):
-    pass
+
+class StartPage():
+    def set_difficulty(value, difficulty):
+        # Do the job here !
+        pass
+
+    def start_the_game():
+        # Сюда писать поссылки на саму игру внутри или сам код
+        pass
+
+    menu = pygame_menu.Menu(300, 400, 'Welcome', theme=pygame_menu.themes.THEME_BLUE)
+
+    menu.add_text_input('Name :', default='John Doe')
+    menu.add_selector("Difficulty :", [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+    menu.add_button('Play', start_the_game)
+    menu.add_button('Quit', pygame_menu.events.EXIT)
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = HomePage()
-    ex.show()
-    sys.exit(app.exec_())
+    pygame.init()
+    surface = pygame.display.set_mode((600, 400))
+    StartP = StartPage()
+    StartP.menu.mainloop(surface)
