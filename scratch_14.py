9529811b7ed28139424037eb24b507f16b3dd26f
Anna <annamyagkova14@gmail.com>
2021-02-01 18:30:32 +0300

    Добавлена доска для шарика (пока не двигается)

--- scratch_14.py
+++ scratch_14.py
@@ -1,8 +1,24 @@
 import pygame, random
+
 FPS = 40
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
+table_border = pygame.sprite.Group()
+lenght_of_table = 20
+table_x = 180
+table_y = 300
+
+
+class Table(pygame.sprite.Sprite):
+    def __init__(self, length):
+        super().__init__(all_sprites)
+        self.len = length
+        self.image = pygame.Surface((2 * length, 2 * length), pygame.SRCALPHA, 32)
+        pygame.draw.rect(self.image, pygame.Color("white"),
+                         (0, 0, 40, length))
+        self.rect = pygame.Rect((180, 300, 40, length))
+        self.add(table_border)
 
 
 class Ball(pygame.sprite.Sprite):
@@ -14,8 +30,9 @@ class Ball(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, pygame.Color("red"),
                            (radius, radius), radius)
         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = 1
-        self.vy = 2
+        self.first_start = True
+        self.vx = 0
+        self.vy = 0
 
     def update(self):
         self.rect = self.rect.move(self.vx, self.vy)
@@ -25,6 +42,15 @@ class Ball(pygame.sprite.Sprite):
         if pygame.sprite.spritecollideany(self, vertical_borders):
             self.vx = -self.vx
             print('/')
+        if pygame.sprite.spritecollideany(self, table_border):
+            print(self.rect.x, self.rect.y)
+            self.vy = -self.vy
+            temp = lenght_of_table / 180
+            print(table_x + lenght_of_table / 2)
+            if self.rect.x < table_x + lenght_of_table / 2:
+                self.vx = -(table_x + lenght_of_table / 2 - self.rect.x) * temp
+            elif self.rect.x > table_x + lenght_of_table / 2:
+                self.vx = (self.rect.x - table_x + lenght_of_table / 2) * temp
 
 
 class Border(pygame.sprite.Sprite):
@@ -49,7 +75,8 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     screen.fill('black')
     running = True
-    ball = Ball(20, 100, 100)
+    ball = Ball(15, 180, 100)
+    table = Table(lenght_of_table)
     clock = pygame.time.Clock()
     Border(5, 5, width - 5, 5)
     Border(5, height - 5, width - 5, height - 5)
@@ -62,6 +89,10 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if ball.first_start is True:
+                    ball.vy = -5
+                    ball.first_start = False
         all_sprites.update()
 
         # Рендеринг
@@ -69,8 +100,3 @@ if __name__ == '__main__':
         all_sprites.draw(screen)
         # После отрисовки всего, переворачиваем экран
         pygame.display.flip()
-
-
-
-
-
